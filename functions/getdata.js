5a96a1067bc32948a87c1aef1d97a2b5dce0049c
chrwiencke <christian@chw.no>
2024-10-25 17:53:52 +0200

    Refactor getdata.js to fetch projects from the database and improve readability

--- functions/getdata.js
+++ functions/getdata.js
@@ -1,36 +1,3 @@
-export default {
-  async fetch(request, env) {
-    const { pathname } = new URL(request.url);
-
-    if (pathname === "/") {
-      const { results } = await env.DB.prepare(
-        "SELECT name, type, link FROM projects"
-      ).all();
-
-      // Create HTML string from the results
-      const projectsHtml = results
-        .map(
-          (project) => `
-          <a class="projectshowcase" href="${project.link}">
-              <div>
-                  <h2>${project.name}</h2>
-                  <p>${project.type}</p>
-              </div>
-          </a>
-        `
-        )
-        .join("");
-
-      return new Response(projectsHtml, {
-        headers: {
-          "Content-Type": "text/html",
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-    }
-
-    return new Response("Visit the root path (/) to see all projects", {
-      status: 404,
-    });
-  },
-};
+export async function onRequestGet(context) {
+    return context.env.SERVICE.fetch(context.request);
+}  
